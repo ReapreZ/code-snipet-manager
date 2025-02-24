310058dfc045bed4e18f301e28021f778d76dd42
Can Kabak <can.kabak.13@gmail.com>
2025-02-24 20:49:01 +0100

    initial commit

---
+++ src/main/java/de/kabak/code_snipet_manager/CodeSnipetManagerApplication.java
@@ -0,0 +1,13 @@
+package de.kabak.code_snipet_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnipetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSnipetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kabak/code_snipet_manager/CodeSnipetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kabak.code_snipet_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSnipetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
